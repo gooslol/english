3b56bd5fd0c71fe0c61f75eae1bc1131cea8959e
iiPython <ben@iipython.dev>
2023-09-01 17:27:15 -0500

    Add get/print builtins, add null, support epilogue

--- english/__main__.py
+++ english/__main__.py
@@ -1,14 +1,13 @@
-# Copyright 2023 iiPython
-# Copyright 2023 DmmD GM
+# Copyright 2023 iiPython & DmmD GM
 
 # Modules
 import sys
 import shlex
 from pathlib import Path
-from typing import Any, List
+from typing import Any, List, Union
 
 # Initialization
-__version__ = "0.0.3"
+__version__ = "0.0.4"
 
 sys.argv = sys.argv[1:]
 if not sys.argv:
@@ -31,13 +30,13 @@ with open(filepath, "r") as fh:
 
 # Handle parsing
 def parse_object(object: str) -> Any:
-    data = object.split(" ")
-    if data[0] == "new":
-        return {"object": dict, "array": list}[data[1]]()
-
-    bool_obj = {"true": True, "false": False}.get(object)
-    if bool_obj is not None:
-        return bool_obj
+    chunks = object.split(" ")
+    handlers = {
+        "new": lambda: {"object": dict, "array": list}[chunks[1]](),
+        "null": lambda: None, "true": lambda: True, "false": lambda: False
+    }
+    if chunks[0] in handlers:
+        return handlers[chunks[0]]()
 
     elif object[0].isdigit() or object[0] in "+-.":
         object = float(object)
@@ -45,6 +44,8 @@ def parse_object(object: str) -> Any:
 
     elif (object[0] == "\"" and object[-1] == "\"") and (len(object) >= 2):
         return object[1:][:-1]
+
+    return object
 
 def cleanup_lines(source: List[str]) -> List[str]:
     return [
@@ -63,10 +64,34 @@ for line_number, line in enumerate(lines):
 
     chapters[data[1]] = line_number + 1
 
+# Mainloop init
+current_line, variables, jumped_prologue, stack = 0, {}, False, []
+
+# Chapter builtins
+def builtin_set(object: str, key: Union[str, int], value: Any) -> None:
+    if object not in variables:
+        return exit("english: no such object exists.")
+
+    object = variables[object]
+    if isinstance(object, list):
+        if isinstance(key, str):
+            return exit("english: an array does not have indexed keys.")
+
+        # Ensure this list is padded enough for you to
+        # index with whatever key we were given
+        object += [None] * ((key or 1) - len(object))
+
+    object[key] = value
+
+builtin_chapters = {
+    "print": lambda *a: print(*[variables.get(_, _) for _ in a]),
+    "set": builtin_set
+}
+
 # Perform mainloop
-current_line, variables, jumped_prologue, stack = 0, {"goos": {}}, False, []
 while current_line < len(lines):
     line, content = lines[current_line], shlex.split(lines[current_line], posix = False)
+    print(f"Line: {current_line} | Content: {line} ({content})")
 
     # If we hit a chapter, skip to prologue
     # This ASSUMES that we already executed all topmost code
@@ -85,7 +110,11 @@ while current_line < len(lines):
     # Handle jumping
     if content[0] == "jump":
         if content[1] == "back":
-            current_line = stack.pop()[0]
+            last_stack = stack.pop()
+            if last_stack[1] == "epilogue":
+                exit()
+
+            current_line = last_stack[0]
             continue
 
         elif content[1] == "last":
@@ -99,7 +128,10 @@ while current_line < len(lines):
         stack.append((current_line + 1, content[2]))
         current_line = chapters.get(content[2])
         if current_line is None:
-            exit("english: cannot jump into the backrooms.")
+            exit(
+                "english: cannot jump into the backrooms."
+                if content[2] != "epilogue" else 0
+            )
 
         continue
 
@@ -116,7 +148,12 @@ while current_line < len(lines):
 
         current[content[is_index - 1]] = parse_object(" ".join(content[is_index + 1:]))
 
+    # Handle builtin chapters
+    if content[0] in builtin_chapters:
+        builtin_chapters[content[0]](
+            *[parse_object(c)
+            for c in content[1:]]
+        )
+
     # Debug info! Wooo!!
-    print(f"Line: {current_line} | Content: {line} ({content})")
-    print(variables)
     current_line += 1
