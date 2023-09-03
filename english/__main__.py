cce58a32c187460011766d8215eabf4a61748e4c
iiPython <ben@iipython.dev>
2023-09-03 16:22:50 -0500

    Add support for variable assignment

--- english/__main__.py
+++ english/__main__.py
@@ -3,13 +3,14 @@
 # Modules
 import re
 import sys
-from typing import List
 from pathlib import Path
+from typing import Any, List
 
 from . import __version__
 from .builtins import (
     builtins, Argument
 )
+from .simple_eval import SimpleEval
 
 # Initialization
 sys.argv = sys.argv[1:]
@@ -27,11 +28,13 @@ if not sys.argv:
 class English(object):
     def __init__(self) -> None:
         self.line = 0
-        self.line_pattern = re.compile(r"(?:\"(.*?)\"|(\S+))")
+        self.line_pattern = re.compile(r"(?:(\".*?\")|(\S+))")
         self.variables = {}
+
         self._default_datatypes = {
             "null": None, "true": True, "false": False
         }
+        self._evaluator = SimpleEval()
 
     def load_file(self, path: str) -> None:
         if not Path(path).is_file():
@@ -53,13 +56,25 @@ class English(object):
         objects = self.line_pattern.findall(line)
         return [obj[0] or obj[1] for obj in objects]
 
+    def eval_expr(self, expr: str) -> Any:
+        parsed_value = self.parse_line([expr])
+        if (len(parsed_value) > 1) or parsed_value[0].raw == parsed_value[0].obj:
+            return self._evaluator.eval(expr, names = self.variables)
+
+        return parsed_value[0].obj
+
     def exec_line(self, line: List[str]) -> None:
-        # print(f"Line: {self.line} | Content: {line}")
+        print(f"Line: {self.line} | Content: {line}")
         builtin = builtins.get(line[0])
         if builtin is not None:
             return builtin(self, *self.parse_line(line[1:]))
 
-        print(line)
+        elif "is" in line:
+            index, item = line.index("is"), self.variables
+            for object in line[:index - 1]:
+                item = item[object.removesuffix("'s")]
+
+            item[line[index - 1]] = self.eval_expr(" ".join(line[index + 1:]))
 
     def parse_line(self, line: List[str]) -> List[Argument]:
         new_line = []
@@ -76,7 +91,7 @@ class English(object):
                 new_line.append(Argument(chunk, self._default_datatypes[chunk]))
 
             else:
-                new_line.append(Argument(chunk, chunk))
+                new_line.append(Argument(chunk, self.variables.get(chunk, chunk)))
 
         return new_line
 
